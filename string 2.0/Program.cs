3a89e6a56160480aad4a563fa20eb519be78bc2b
Emil Stener <stener99@hotmail.se>
2016-10-14 14:03:45 +0200

    hej

--- string 2.0/Program.cs
+++ string 2.0/Program.cs
@@ -7,9 +7,50 @@ using System.Threading.Tasks;
 namespace string_2._0
 {
     class Program
-    {
+    {   //övning1
+       
         static void Main(string[] args)
-        {
+        {  /*
+            DateTime dt =DateTime.Now;
+            Console.WriteLine(dt.ToString("d"));
+            Console.ReadKey();
+            
+
+            //övning2
+            double pi = Math.PI;
+            double aria = 4 * 4 * pi;
+            Console.WriteLine(aria.ToString("F4"));
+            Console.ReadKey();
+
+            
+
+
+            //övning3
+            Console.WriteLine("{0:000,000,000}",1000000000);
+            Console.ReadKey();
+            
+           
+            //övning4
+            double tal = 123.000;
+            Console.WriteLine("de blir " "{0} kr och 00 öre ", tal);
+
+            Console.ReadKey();
+             */
+
+            //övning5
+
+            Console.WriteLine();
+            var
+            Console.WriteLine("{0:####-##-##-####}", a ); 
+            
+
+            Console.ReadKey();
+
+
+
         }
+
+
+
     }
 }
